9312c74c594d852dcd888df5bd08c472786ef1c4
Favour Shaw <favourvanderwaal@gmail.com>
2025-11-17 10:29:39 +0100

    Implement tab navigation and layout structure for the app

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,48 @@
+import { Ionicons } from "@expo/vector-icons";
+import { Tabs } from "expo-router";
+import React from "react";
+
+const TabsLayout = () => {
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: "tomato",
+        tabBarInactiveTintColor: "gray",
+        tabBarStyle: {
+          paddingBottom: 30,
+          paddingTop: 10,
+          height: 90,
+          backgroundColor: "blue",
+          borderTopWidth: 1,
+          borderTopColor: "yellow",
+        },
+        tabBarLabelStyle: {
+          fontSize: 12,
+          fontWeight: 600,
+        },
+        headerShown: false,
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Todos",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="flash-outline" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="settings"
+        options={{
+          title: "Settings",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="settings" size={size} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+};
+
+export default TabsLayout;
