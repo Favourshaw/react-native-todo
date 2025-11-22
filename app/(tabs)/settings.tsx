ceca075ecc9f8ad416297ae6f68d0d39eb2da0e2
Favour Shaw <favourvanderwaal@gmail.com>
2025-11-22 21:02:01 +0100

    feat: add settings screen with auto-sync and notifications options, and integrate ProgressStats component

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,11 +1,46 @@
-import React from "react";
-import { Text, View } from "react-native";
+import { createSettingsStyles } from "@/assets/styles/settings.style";
+import ProgressStats from "@/components/ProgressStats";
+import useTheme from "@/hooks/useTheme";
+import { Ionicons } from "@expo/vector-icons";
+import { LinearGradient } from "expo-linear-gradient";
+import React, { useState } from "react";
+import { ScrollView, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const settingsScreen = () => {
+  const [isAutoSync, setIsAutoSync] = useState(false);
+  const [isNotificationsEnabled, setIsNotificationsEnabled] = useState(true);
+
+  const { colors, isDarkMode, toggleDarkMode } = useTheme();
+
+  const settingStyles = createSettingsStyles(colors);
   return (
-    <View>
-      <Text>settingsScreen</Text>
-    </View>
+    <LinearGradient
+      colors={colors.gradients.background}
+      style={settingStyles.container}
+    >
+      <SafeAreaView style={settingStyles.safeArea}>
+        <View style={settingStyles.header}>
+          <View style={settingStyles.titleContainer}>
+            <LinearGradient
+              colors={colors.gradients.primary}
+              style={settingStyles.iconContainer}
+            >
+              <Ionicons name="settings" size={28} color="#fff" />
+            </LinearGradient>
+            <Text style={settingStyles.title}>Settings</Text>
+          </View>
+        </View>
+
+        <ScrollView
+          style={settingStyles.scrollView}
+          contentContainerStyle={settingStyles.content}
+          showsVerticalScrollIndicator={false}
+        >
+          <ProgressStats />
+        </ScrollView>
+      </SafeAreaView>
+    </LinearGradient>
   );
 };
 
